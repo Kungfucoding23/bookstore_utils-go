4d2bad92ffb8fe216c0098c67f3fb0187e439928
Alejandro <alesantin012@hotmail.com>
2021-03-22 10:16:01 -0300

    RestErr interface

--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -11,40 +11,48 @@ import (
 func TestNewInternalServerError(t *testing.T) {
 	err := NewInternalServerError("this is the message", errors.New("internal error"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "internal_server_error", err.Error)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
+	// assert.EqualValues(t, "internal_server_error", err.Error())
 	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes))
-	assert.EqualValues(t, "internal error", err.Causes[0])
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "internal error", err.Causes()[0])
 }
 func TestNewBadRequestError(t *testing.T) {
 	err := NewBadRequestError("this is the message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusBadRequest, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "bad_request", err.Error)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
+	// assert.EqualValues(t, "bad_request", err.Error)
 }
 func TestNewNotFoundError(t *testing.T) {
 	err := NewNotFoundError("this is the message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusNotFound, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "not_found", err.Error)
+	assert.EqualValues(t, http.StatusNotFound, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
+	// assert.EqualValues(t, "not_found", err.Error)
 }
 
 func TestNewRestError(t *testing.T) {
 	err := NewRestError("this is the message", http.StatusNotImplemented, "not_implemented", nil)
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusNotImplemented, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "not_implemented", err.Error)
-	assert.Nil(t, err.Causes)
+	assert.EqualValues(t, http.StatusNotImplemented, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
+	// assert.EqualValues(t, "not_implemented", err.Error)
+	assert.Nil(t, err.Causes())
 }
 func TestNewUnauthorizedError(t *testing.T) {
 	err := NewUnauthorizedError("this is the message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusUnauthorized, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "unauthorized", err.Error)
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
+	// assert.EqualValues(t, "unauthorized", err.Error)
 }
+
+// func TestNewRestErrorFromBytes(t *testing.T) {
+// 	var bytes []byte
+// 	restErr, err := NewRestErrorFromBytes(bytes)
+// 	assert.NotNil(t, err)
+// 	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+// 	assert.EqualValues(t, "this is the message", err.Message())
+// }
